8239e4bf8d9019f956aa73d59032c58457580a05
Alexander Cole <alu0100767421@ull.edu.es>
2017-02-11 00:47:58 +0000

    arreglado problema con las rutas

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
 var port = process.env.PORT || 8080;
-
+var path = require("path");
 
 var app = express();
 
 app.set(port);
-app.use(express.static(__dirname+'/html/'));
 
-app.get('/', function(request, response){
-  response.sendFile('/html/index.html');
+app.use(express.static(__dirname ));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/index.html'));
 });
-
 
 
 app.listen(port, function ()
