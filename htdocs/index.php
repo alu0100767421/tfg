1418e7058dd7c379aaaf78450efb093c8c9ff004
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-26 14:00:23 +0100

    cambios

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,12 +3,40 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Sistema Geográfico de Paleontología de Canarias</title>
+
     <link rel="icon" type="/image/png" href="../images/logoULL/logotipo-secundario.jpg" />
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="css/index.css"/>
+
   </head>
   <body>
-    <h1>Hello world!</h1>
-    <a href="pages/mapa.php">mapa info</a>
+    <nav class="navbar navbar-inverse bg-primary">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="cuerpo">
+      <div class="titulo">
+        <h1>Sistema Geográfico de Paleontología de Canarias</h1>
+      </div>
+      <div class="acceso">
+        <a class="btn btn-success" href="pages/mapa.php" role="button">Mapa Paleontológico</a>
+        <p>hehjkjhgjjhujye</p>
+      </div>
+    </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/index.js"></script>
   </body>
 </html>
