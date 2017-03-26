994aada1514e71eebeac7fa57b6737eb2ce57975
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-26 11:19:10 +0100

    comenzando landing page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,14 @@
-
-<html><body><h1>Hello world!</h1>
-<a href="pages/mapa.php">mapa info</a></body></html>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema Geográfico de Paleontología de Canarias</title>
+    <link rel="icon" type="/image/png" href="../images/logoULL/logotipo-secundario.jpg" />
+    <link type="text/css" rel="stylesheet" href="css/index.css"/>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+    <a href="pages/mapa.php">mapa info</a>
+  </body>
+</html>
