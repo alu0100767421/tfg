d2d51cc2572456b1b45236456124adb941d2d0de
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-04-04 21:53:23 +0100

    cambios

--- htdocs/pages/administracion.php
+++ htdocs/pages/administracion.php
@@ -15,7 +15,7 @@
 
     <link rel="icon" type="/image/png" href="../images/logoULL/logotipo-secundario-ULL.png" />
     <link type="text/css" rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="../css/index.css"/>
+    <link type="text/css" rel="stylesheet" href="../css/administracion.css"/>
 
   </head>
   <body>
@@ -36,26 +36,37 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="pages/mapa.php">Mapa</a></li>
+            <li><a href="../index.php">Inicio</a></li>
+            <li><a href="mapa.php">Mapa</a></li>
           </ul>
         </div>
 
       </div>
     </nav>
-    <!--FIN DE MENU-->
-    <div class="cuerpo">
-      <div class="row">
-        <div class="col-lg-2 col-md-4 col-xs-2 col-sm-2 titulo">
-          <h1>Administración</h1>
-        </div>
+
+    <div class="containter">
+      <div class="col-lg-2 col-md-4 col-xs-12 col-sm-12 panel">
+
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Consultar BBDD</a></li>
+          <li><a href="#">Añadir BBDD</a></li>
+          <li><a href="#">Gestión Usuarios</a></li>
+
+        </ul>
+      </div>
+      <div class="col-lg-10 col-md-8 col-xs-12 col-sm-12 contenido">
+        <h2>Contenido</h2>
       </div>
 
     </div>
 
 
+
+
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script type="text/javascript" src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="../js/index.js"></script>
+  <script type="text/javascript" src="../js/administracion.js"></script>
   </body>
 </html>
