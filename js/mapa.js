b637c15356b23b592cfefa1697a153aa97fe4b88
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-06-14 23:30:15 +0100

    intenando arreglar escala responsive

--- js/mapa.js
+++ js/mapa.js
@@ -1,4 +1,4 @@
-d3.select(window).on('resize', resize);
+d3.select(window).on('resize', sizeChange);
 
 
 //Creamos unas dimensiones específicas para el mapa
@@ -6,11 +6,12 @@ d3.select(window).on('resize', resize);
   var viewportHeight = $("#mapa").height()/2;
   var width = viewportWidth * 0.97;
   var height = width/1.85;
-
-  var escala=width*10;
+  var escala= width*10;
+  console.log(escala);
+  //var escala=10000;
 //Projeccion del mapa
 var projection = d3.geo.mercator()
-    .scale([width*10]) //escala
+    .scale([escala]) //escala
     .center([-15.747476639999999,28.530921143001386])//para que se centre
     .translate([width/2,height/2]);
 
@@ -53,7 +54,7 @@ d3.json("../mapas/mapa_modificado/recintos_municipales_inspire_canarias_wgs84.ge
 
 function puntos() {
 
-  // add circles to svg
+  // añadir puntos al svg
    svg.selectAll("circle")
    .data(array_puntos)
    .enter()
@@ -118,66 +119,53 @@ function mostrarYacimiento(d) {
 }
 
 function quitarYacimiento(d) {
-  // Use D3 to select element, change color back to normal
   d3.select(this).attr({
     fill: "red",
     r: "4px",
     stroke: "black"
   });
-
-  // Select text by id and then remove
-  d3.select("#id"+d.idyacimiento).remove();  // Remove text location
+  d3.select("#id"+d.idyacimiento).remove();
 }
 
-function puntos_zoom() {
-  /*aa = [-16.79612159729004, 28.236800677752584];
-  bb = [-16.718788146972656, 28.229389972499913];
 
-  svg.selectAll("circle")
-  .data([aa,bb,cc]).enter()
-  .append("circle")
-  .attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
-  .attr("cy", function (d) { return projection(d)[1]; })
-  .attr("r", "3px")
-  .attr("fill", "orange");*/
-  var municipio= document.getElementById('Municipio').value;
-
-  // add circles to svg
-
-   /*svg.selectAll("circle")
-   .data(place)
-   .enter()
-   .append("circle")
-   .attr("r", "3px")
-   .attr("fill", "orange")
-   .attr("transform", function(d) {
-    return "translate(" + projection([
-      d.location.longitude,
-      d.location.latitude
-    ]) + ")";
-  });*/
-}
 
 
 function resize() {
 
+  //Creamos unas dimensiones específicas para el mapa
+    var viewportWidth = $("#mapa").width();
+    var viewportHeight = $("#mapa").height()/2;
+    var width = viewportWidth * 0.97;
+    var height = width/1.85;
+    var escala= width*10;
+    //var escala=10000;
+  //Projeccion del mapa
+  var projection = d3.geo.mercator()
+      .scale([escala]) //escala
+      .center([-15.747476639999999,28.530921143001386])//para que se centre
+      .translate([width/2,height/2]);
 
-  var viewportWidth = $("#mapa").width();
-  var viewportHeight = $("#mapa").height()/2;
-  var width = viewportWidth * 0.97;
-  var height = width/1.85;
-
-   projection
-    	.scale([width*10])
-      .center([-15.747476639999999,28.530921143001386]) //projection center
-   		.translate([width/2,height*2]);
+  //Generate paths based on projection
+  var path = d3.geo.path().projection(projection);
 
 
-   d3.select("#mapa").attr("width",width).attr("height",height);
-   d3.select("svg").attr("width",width).attr("height",height);
-
-   d3.selectAll("path").attr('d', path);
+  //Creamos el SVG
+  var svg = d3.select("#mapa").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 }
+
+function sizeChange() {
+  svg.selectAll("circle").remove();
+  escala=($("#mapa").width()*0.97)*10;
+  console.log("Escala de prueba:"+escala);
+  d3.select("g").attr("transform","scale(" + $("#mapa").width()/1000 + ")","translate(" + (-15.747476639999999+width/2) + "," + (28.530921143001386+height/2) + ")");
+  $("#mapa").height($("#mapa").width()*1.85);
+  //puntos();
+
+
+}
+
 
 // Zoom al clickar
 function clicked(d,i) {
@@ -218,7 +206,7 @@ function clicked(d,i) {
   if(k!=1){
     svg.selectAll("circle").remove();
     document.getElementById('Municipio').value=municipio_seleccionado;
-    puntos_zoom();
+    puntos();
     console.log("k="+k);
     console.log("x="+x);
     console.log("y="+y);
